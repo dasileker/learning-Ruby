22a5b6ed2601098e40643c4c80dc145c89ce54ed
dasileker <dasileker@gmail.com>
2023-02-01 19:54:10 +0100

    add the exercise

--- comment.rb
+++ comment.rb
@@ -20,3 +20,13 @@
 # Computer programs exist to quickly analyze and manipulate data. For that reason, it’s important for us to understand the different data types that we can use in our programs.
 
 # Reminder: never use quotation marks (‘ or “) with booleans, or Ruby will think you’re talking about a string (a word or phrase) instead of a value that can be true or false. It’s also important to remember that Ruby is case-sensitive (it cares about capitalization).
+
+my_num =   25  # Add your code here!
+
+my_boolean =   true  # And here!
+
+my_string =   "Ruby"  # Also here.
+
+puts my_num
+puts my_boolean
+puts my_string

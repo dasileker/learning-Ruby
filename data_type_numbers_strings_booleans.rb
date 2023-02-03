edde595d9fc912693ba958a781104b0fa1ee4639
dasileker <dasileker@gmail.com>
2023-02-03 20:04:23 +0100

    add logic

--- data_type_numbers_strings_booleans.rb
+++ data_type_numbers_strings_booleans.rb
@@ -12,3 +12,15 @@
 # my_num to the value 25
 # my_boolean to the value true (note the capitalization!)
 # my_string to the value "Ruby"
+
+# code 
+
+my_num =   25  # Add your code here!
+
+my_boolean =   true  # And here!
+
+my_string =   "Ruby"  # Also here.
+
+puts my_num
+puts my_boolean
+puts my_string

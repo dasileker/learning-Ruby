5c44eac0c3eaa47a2cd345aea3ffe9d099496113
dasileker <dasileker@gmail.com>
2023-02-21 18:33:59 +0100

    adding the lesson script

---
+++ Prompting_the_user.rb
@@ -0,0 +1,9 @@
+# PUTTING THE FORM IN FORMATTER
+# Prompting the User
+# First, let’s write the code we’re already familiar with. In order to get input from the user, we’ll first need to print a prompt on the screen.
+
+# Instructions
+# 1.
+# print the question "What's your first name?" to the screen. Feel free to peek back at the first exercise if you need a syntax reminder.
+
+

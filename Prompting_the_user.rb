6bd486509fd4a0b79bb3649384beee3cf26efb99
dasileker <dasileker@gmail.com>
2023-02-21 18:40:04 +0100

    adding codes

--- Prompting_the_user.rb
+++ Prompting_the_user.rb
@@ -7,3 +7,15 @@
 # print the question "What's your first name?" to the screen. Feel free to peek back at the first exercise if you need a syntax reminder.
 
 
+print "what's you first name!: "
+name = gets.chomp
+name.capitalize!
+
+print "What's your last name!: "
+last_name = gets.chomp
+last_name.capitalize!
+
+print "What's your age!: "
+age = gets.chomp
+
+puts "Your first name is #{name}, and your last name is #{last_name}, and your age is #{age}"

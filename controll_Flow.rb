2f9747ac586127f5af5116a13cbdbef0c9db351c
dasileker <dasileker@gmail.com>
2023-04-02 12:16:15 +0000

    adding the code

--- controll_Flow.rb
+++ controll_Flow.rb
@@ -27,4 +27,15 @@ action! (Go ahead and give Ruby an integer—that is, a positive or negative num
 =end
 
 
+print "Integer please: "
+user_num = Integer(gets.chomp)
+
+if user_num < 0
+  puts "You picked a negative integer!"
+elsif user_num > 0
+  puts "You picked a positive integer!"
+else
+  puts "You picked zero!"
+end
+
 
